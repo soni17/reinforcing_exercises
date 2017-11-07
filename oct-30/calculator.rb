eb1b51f2cba719a559d95d9dfdd3548ad142c3c2
soni17 <elb004@gmail.com>
2017-11-07 18:43:39 -0500

    Oct 30 reinforcing exercise

---
+++ oct-30/calculator.rb
@@ -0,0 +1,18 @@
+
+def add(arg1,arg2)
+  return arg1 + arg2
+end
+
+
+def subtract(arg1,arg2)
+  return arg1-arg2
+end
+
+
+def sum(arg)
+  total = 0
+  arg.each do |num|
+    total += num
+  end
+  return total
+end
