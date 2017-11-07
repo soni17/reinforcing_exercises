cb761389b16b1b87bed0d5291c90085a2be0754b
soni17 <elb004@gmail.com>
2017-11-07 18:57:10 -0500

    Oct 31 - simplified friend class

--- oct-31/friend.rb
+++ oct-31/friend.rb
@@ -1,13 +1,6 @@
 class Friend
 
-  def greet(name="Hello !")
-
-    if name != "Hello !"
-      return "Hello #{name}!"
-    else
-      return name
-    end
-
+  def greet(name = '')
+    "Hello #{name}!"
   end
-
 end
