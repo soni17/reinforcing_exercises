767aadcd71746f2b32640b00d64694c6660fe9ab
soni17 <elb004@gmail.com>
2017-11-07 23:45:05 -0500

    Nov-3 reinforcing exercises

---
+++ Nov-3/dictionary.rb
@@ -0,0 +1,35 @@
+class Dictionary
+
+  def initialize
+    @words = {}
+  end
+
+  def entries
+    return @words
+  end
+
+  def keywords
+    return @words.keys.sort
+  end
+
+  def add(word)
+
+    if word.class == Hash
+      @words.merge!(word)
+    elsif word.class == String
+      @words.merge!( {word => nil} )
+    end
+
+  end
+
+  def include?(lookup)
+    keywords.include?(lookup)
+  end
+
+  def find(lookup)
+    @words.select{ |k,v|
+      k.start_with?(lookup)
+    }
+  end
+
+end
